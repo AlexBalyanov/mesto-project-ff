8ac9293e0389aac76ab58ef48128e3b047448bb5
Alexander Balyanov <alex44298@yandex.ru>
2025-04-18 14:19:46 +0500

    сделал функцию добавления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,42 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placeCards = (cardsArray) => {
+  const placeList = document.querySelector(".places__list");
+  const cardTemplate = document.querySelector("#card-template").content;
+
+  cardsArray.forEach((card) => {
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+    cardElement.querySelector(".card__title").textContent = card.name;
+    cardElement.querySelector(".card__image").src = card.link;
+
+    placeList.append(cardElement);
+  });
+};
+
+const deletCard = (card) => {
+  const deletButton = document.querySelector(".card__delete-button");
+
+  deletButton.addEventListener("click", () => {
+    card.remove();
+  });
+};
+
+placeCards(initialCards);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(cardElement)
