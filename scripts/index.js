b434455a8a18453964a889975e98d8e01fa0534a
Alex Balyanov <alex44298@yandex.ru>
2025-04-19 18:19:30 +0500

    исправил замечания по ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,30 @@
-const placeCards = (cardsArray, deleteCardCallback) => {
+const placeList = document.querySelector(".places__list");
 
-  cardsArray.forEach((card) => {
-    const placeList = document.querySelector(".places__list");
-    const cardTemplate = document.querySelector("#card-template").content;
-    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+const createCard = (dataFromCards, onDeleteCardCallback) => {
 
-    cardElement.querySelector(".card__title").textContent = card.name;
-    cardElement.querySelector(".card__image").src = card.link;
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  
+  cardElement.querySelector(".card__title").textContent = dataFromCards.name
+  cardElement.querySelector(".card__image").src = dataFromCards.link;
+  cardElement.querySelector(".card__image").alt = `Фотография места: ${dataFromCards.name}`;
+  
+  cardElement.querySelector(".card__delete-button").addEventListener("click", () => {
+    onDeleteCardCallback(cardElement);  
+  });
 
-    placeList.append(cardElement);
+  return cardElement;
+};
 
-    cardElement.querySelector(".card__delete-button").addEventListener("click", () => {
-      deleteCardCallback(cardElement);
-    });
+const placeCards = () => {
+  initialCards.forEach((cardItem) => {
+    const cardData = createCard(cardItem, deleteCard);
+    placeList.append(cardData);
   });
 };
 
 const deleteCard = (cardElement) => {
   cardElement.remove();
-}
+};
 
-placeCards(initialCards, deleteCard);
+placeCards();
